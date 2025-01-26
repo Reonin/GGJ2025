f6c36ce34c323c0ac9b980bd46d83feeb8ff2843
andross <blake.balick.schreiber@gmail.com>
2025-01-26 14:00:05 -0500

    Add new obstacle
    - Add shark obstacle
    - Made obstacle an inheritable class

--- js/Obstacle.js
+++ js/Obstacle.js
@@ -1,20 +1,29 @@
 import { URCHIN_START_Z } from './Constants.js';
 
 export class Obstacle {
-    constructor(Babylon, scene, textureObj) {
+    constructor(Babylon, scene, textureObj, config = {}) {
         this.BABYLON = Babylon;
         this.scene = scene;
         this.textureObj = textureObj;
         this.listOfPointsActive = [];
         this.direction = true;
 
-        this.mesh = this.BABYLON.MeshBuilder.CreateSphere("obstacle", { diameter: 2 }, this.scene);
-        this.mesh.billboardMode = this.BABYLON.Mesh.BILLBOARDMODE_ALL;
+        this.type = "obstacle";
+        this.mesh = config.mesh || this.BABYLON.MeshBuilder.CreateSphere("urchin", { diameter: 2 }, this.scene);
+        this.mesh.billboardMode = config.billboard || this.BABYLON.Mesh.BILLBOARDMODE_ALL;
         this.mesh.position.x = -15;
         this.mesh.position.y = 0;
-        this.mesh.position.z = 5;
-        this.mesh.material = this.textureObj.urchin_texture;
+        this.mesh.position.z = config.startZ || 5;
+        this.mesh.material = config.texture || this.textureObj.urchin_texture;
         setInterval(this.positionObstacle.bind(this), 10);
+    }
+
+    setStartPosition(startZ){
+        this.mesh.position.z = startZ;
+    }
+
+    setMesh(mesh){
+        this.mesh = mesh;
     }
 
     positionObstacle(){
--- js/ObstacleFactory.js
+++ js/ObstacleFactory.js
@@ -1,5 +1,6 @@
 import { URCHIN_START_Z } from './Constants.js';
-import { Obstacle } from './Obstacle.js';
+import { Urchin } from './Urchin.js';
+import { Shark } from './Shark.js';
 
 export class ObstacleFactory {
     constructor(Babylon, scene, textureObj) {
@@ -11,11 +12,20 @@ export class ObstacleFactory {
       this.scene.onBeforeRenderObservable.add(this.checkAndDestroyStaleMeshes.bind(this));
 
       setInterval( this.createUrchin.bind(this) , 3000);
+      setInterval( this.createShark.bind(this), 5000);
     }
     createUrchin(){
         if(Math.random() >= 0.5) { // experiment with delta time instead
-            const urchin = new Obstacle(this.BABYLON, this.scene, this.textureObj);
+            const urchin = new Urchin(this.BABYLON, this.scene, this.textureObj, {});
             this.listOfPointsActive.push(urchin);
+
+        }
+    }
+    createShark(){
+        if(Math.random() >= 0.5) { // experiment with delta time instead
+            const shark = new Shark(this.BABYLON, this.scene, this.textureObj, {});
+            console.log(`Spawning shark at ${shark.mesh.position}`);
+            this.listOfPointsActive.push(shark);
 
         }
     }
